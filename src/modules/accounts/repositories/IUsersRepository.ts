06524e94513d29f2038a45930572dde154489204
carlos-hr <carlos.henrique17.chr@gmail.com>
2022-08-31 20:30:21 -0300

    adding hash to password

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,5 +1,7 @@
 import { ICreateUserDTO } from '../dtos/ICreateUser';
+import { User } from '../model/User';
 
 export interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<void>;
+  findByEmail(email: string): Promise<User>;
 }
--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from 'bcryptjs';
 import { inject, injectable } from 'tsyringe';
 
 import { ICreateUserDTO } from '../../dtos/ICreateUser';
@@ -13,11 +14,19 @@ export class CreateUserUseCase {
   async execute(data: ICreateUserDTO) {
     const { name, email, password, driver_license } = data;
 
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists');
+    }
+
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
       driver_license,
       email,
       name,
-      password,
+      password: passwordHash,
     });
   }
 }
