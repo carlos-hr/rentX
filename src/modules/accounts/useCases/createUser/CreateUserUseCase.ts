19cea81e4e215c66799b8b8e7c38bf2fe551e893
carlos-hr <carlos.henrique17.chr@gmail.com>
2022-09-01 09:09:01 -0300

    handling errors

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { hash } from 'bcryptjs';
 import { inject, injectable } from 'tsyringe';
 
+import { AppError } from '../../../../errors/AppError';
 import { ICreateUserDTO } from '../../dtos/ICreateUser';
 import { IUsersRepository } from '../../repositories/IUsersRepository';
 
@@ -17,7 +18,7 @@ export class CreateUserUseCase {
     const userAlreadyExists = await this.usersRepository.findByEmail(email);
 
     if (userAlreadyExists) {
-      throw new Error('User already exists');
+      throw new AppError('User already exists', 400);
     }
 
     const passwordHash = await hash(password, 8);
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
+import { AppError } from './errors/AppError';
 import { router } from './routes';
 import swaggerSetup from './swagger.json';
 import './database';
@@ -14,5 +17,15 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSetup));
 
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+
+  return res
+    .status(500)
+    .json({ message: `Internal server error - ${err.message}` });
+});
 
 app.listen(3333, () => console.log('Server running!'));
