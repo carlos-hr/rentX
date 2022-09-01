1cb3e9d12269a57af231d0383185902b1d68effd
carlos-hr <carlos.henrique17.chr@gmail.com>
2022-09-01 08:42:40 -0300

    adding middleware to verify authentication in routes

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -4,4 +4,5 @@ import { User } from '../model/User';
 export interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<void>;
   findByEmail(email: string): Promise<User>;
+  findById(id: string): Promise<User>;
 }
