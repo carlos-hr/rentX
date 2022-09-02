314f892e1b4cfae78bc2285c6821254e8865878b
carlos-hr <carlos.henrique17.chr@gmail.com>
2022-09-02 16:22:24 -0300

    fix: adjusting imports

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,6 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
-import { AppError } from '../../../../errors/AppError';
+import { AppError } from '@errors/AppError';
 import { ISpecificationsRepository } from '../../repositories/specifications/ISpecificationsRepository';
 
 interface IRequest {
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,11 +1,11 @@
 import { container } from 'tsyringe';
 
-import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
-import { UsersRepository } from '../../modules/accounts/repositories/UsersRepository';
-import { CategoriesRepository } from '../../modules/cars/repositories/categories/CategoriesRepository';
-import { ICategoriesRepository } from '../../modules/cars/repositories/categories/ICategoriesRepository';
-import { ISpecificationsRepository } from '../../modules/cars/repositories/specifications/ISpecificationsRepository';
-import { SpecificationsRepository } from '../../modules/cars/repositories/specifications/SpecificationRepository';
+import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { UsersRepository } from '@modules/accounts/repositories/UsersRepository';
+import { CategoriesRepository } from '@modules/cars/repositories/categories/CategoriesRepository';
+import { ICategoriesRepository } from '@modules/cars/repositories/categories/ICategoriesRepository';
+import { ISpecificationsRepository } from '@modules/cars/repositories/specifications/ISpecificationsRepository';
+import { SpecificationsRepository } from '@modules/cars/repositories/specifications/SpecificationRepository';
 
 container.registerSingleton<ICategoriesRepository>(
   'CategoriesRepository',
