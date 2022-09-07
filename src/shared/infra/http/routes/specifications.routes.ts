647da6db0177b5649080c095a2cb466cbebe426f
carlos-hr <carlos.henrique17.chr@gmail.com>
2022-09-06 21:43:49 -0300

    creating middlewares to verify if user is admin and applying to routes

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -3,14 +3,18 @@ import { Router } from 'express';
 import { verifyAuthentication } from '@shared/infra/http/middlewares/verifyAuthentication';
 import { CreateSpecificationController } from '@modules/cars/useCases/createSpecification/CreateSpecificationController';
 import { ListSpecificationsController } from '@modules/cars/useCases/listSpecifications/ListSpecificationsController';
+import { ensureAdmin } from '../middlewares/ensureAdmin';
 
 export const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 const listSpecificationsController = new ListSpecificationsController();
 
-specificationsRoutes.use(verifyAuthentication);
-
-specificationsRoutes.post('/', createSpecificationController.handle);
+specificationsRoutes.post(
+  '/',
+  verifyAuthentication,
+  ensureAdmin,
+  createSpecificationController.handle
+);
 
 specificationsRoutes.get('/', listSpecificationsController.handle);
