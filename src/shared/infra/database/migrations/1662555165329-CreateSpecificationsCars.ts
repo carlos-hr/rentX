6b130395ddc463c9d3f8316306614f69426f5631
carlos-hr <carlos.henrique17.chr@gmail.com>
2022-09-07 10:03:17 -0300

    adding migrations to relation table between specifications and cars

---
+++ src/shared/infra/database/migrations/1662555165329-CreateSpecificationsCars.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateSpecificationsCars1662555165329
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'specifications_cars',
+        columns: [
+          {
+            name: 'car_id',
+            type: 'uuid',
+          },
+          {
+            name: 'specification_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('specifications_cars');
+  }
+}
