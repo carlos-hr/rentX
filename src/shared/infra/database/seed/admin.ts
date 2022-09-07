0500bb907df5ec8762cfee81bea5be77f10a62d8
carlos-hr <carlos.henrique17.chr@gmail.com>
2022-09-06 21:00:41 -0300

    creating admin seed

---
+++ src/shared/infra/database/seed/admin.ts
@@ -0,0 +1,18 @@
+import { hash } from 'bcryptjs';
+import { v4 } from 'uuid';
+import createConnection from '..';
+
+async function create() {
+  const connection = await createConnection();
+
+  const id = v4();
+  const password = await hash('admin', 8);
+
+  await connection.query(
+    `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
+            values('${id}', 'admin', 'admin@rentx.com', '${password}', true, 'now()', 'xxxx')
+        `
+  );
+}
+
+create().then(() => console.log('Admin created'));
