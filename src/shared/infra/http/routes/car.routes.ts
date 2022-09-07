3117507aec17e786395addabd8e230d6e9620a30
carlos-hr <carlos.henrique17.chr@gmail.com>
2022-09-07 16:42:04 -0300

    creating use case to add images on a car

--- src/shared/infra/http/routes/car.routes.ts
+++ src/shared/infra/http/routes/car.routes.ts
@@ -1,9 +1,12 @@
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
 import { CreateCarSpecificationController } from '@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController';
 import { ListCarsController } from '@modules/cars/useCases/listCars/ListCarsController';
+import { UploadCarImagesController } from '@modules/cars/useCases/uploadCarImages/UploadCarImagesController';
 import { Router } from 'express';
+import multer from 'multer';
 import { ensureAdmin } from '../middlewares/ensureAdmin';
 import { verifyAuthentication } from '../middlewares/verifyAuthentication';
+import uploadConfig from '@config/upload';
 
 export const carsRoutes = Router();
 
@@ -11,6 +14,9 @@ const createCarController = new CreateCarController();
 const listCarsController = new ListCarsController();
 const createCarsSpecificationsController =
   new CreateCarSpecificationController();
+const uploadCarImagesController = new UploadCarImagesController();
+
+const uploadImages = multer(uploadConfig.upload('./tmp/cars'));
 
 carsRoutes.post(
   '/',
@@ -23,7 +29,15 @@ carsRoutes.get('/', listCarsController.handle);
 
 carsRoutes.post(
   '/specifications/:id',
-  // verifyAuthentication,
-  // ensureAdmin,
+  verifyAuthentication,
+  ensureAdmin,
   createCarsSpecificationsController.handle
+);
+
+carsRoutes.post(
+  '/images/:car_id',
+  verifyAuthentication,
+  ensureAdmin,
+  uploadImages.array('images'),
+  uploadCarImagesController.handle
 );
