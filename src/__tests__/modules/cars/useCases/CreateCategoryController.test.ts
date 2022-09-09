9834c825b0c0a68deaf005a6f1a90b60dc2473d5
carlos-hr <carlos.henrique17.chr@gmail.com>
2022-09-08 22:11:26 -0300

    creating intregration test to create category

---
+++ src/__tests__/modules/cars/useCases/CreateCategoryController.test.ts
@@ -0,0 +1,50 @@
+import request from 'supertest';
+import { app } from '@shared/infra/http/app';
+import { Connection } from 'typeorm';
+import createConnection from '@shared/infra/database';
+import { hash } from 'bcryptjs';
+import { v4 } from 'uuid';
+
+let connection: Connection;
+
+describe('Create Category Controller', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = v4();
+    const password = await hash('admin', 8);
+
+    await connection.query(
+      `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
+      values('${id}', 'admin', 'admin@rentx.com', '${password}', true, 'now()', 'xxxx')
+    `
+    );
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it('should be able to create a new category ', async () => {
+    const responseToken = await request(app).post('/auth').send({
+      email: 'admin@rentx.com',
+      password: 'admin',
+    });
+
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+      .post('/categories')
+      .send({
+        name: 'teste',
+        description: 'testeteste',
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.status).toBe(201);
+  });
+});
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,32 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import 'reflect-metadata';
-import express, { NextFunction, Request, Response } from 'express';
-import 'express-async-errors';
-import swaggerUi from 'swagger-ui-express';
-
-import { AppError } from '@errors/AppError';
-import { router } from './routes';
-import swaggerSetup from '../../../swagger.json';
-import '@shared/container';
-import createConnection from '@shared/infra/database';
-
-createConnection();
-const app = express();
-
-app.use(express.json());
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSetup));
-
-app.use(router);
-
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof AppError) {
-    return res.status(err.statusCode).json({ message: err.message });
-  }
-
-  return res
-    .status(500)
-    .json({ message: `Internal server error - ${err.message}` });
-});
+import { app } from './app';
 
 app.listen(3333, () => console.log('Server running!'));
