40ce77ac98feb29d7eae2e78af0a783a944f1d95
carlos-hr <carlos.henrique17.chr@gmail.com>
2022-10-03 15:40:26 -0300

    fixing method to find avaiable car

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -9,11 +9,21 @@ export class RentalsRepository implements IRentalsRepository {
   }
 
   async findOpenRentalByCarId(car_id: string): Promise<Rental> {
-    return this.repository.findOne({ car_id });
+    return this.repository.findOne({
+      where: {
+        car_id,
+        end_date: null,
+      },
+    });
   }
 
   async findOpenRentalByUserId(user_id: string): Promise<Rental> {
-    return this.repository.findOne({ user_id });
+    return this.repository.findOne({
+      where: {
+        user_id,
+        end_date: null,
+      },
+    });
   }
 
   async create({
