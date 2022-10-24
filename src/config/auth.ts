e7f1ad13c9108f4e2989bb25c02924e83c23422b
carlos-hr <carlos.henrique17.chr@gmail.com>
2022-10-24 19:37:14 -0300

    feat: adding refresh token as response for login

---
+++ src/config/auth.ts
@@ -0,0 +1,8 @@
+export default {
+  secret_token: '073dba17fba0dc86eca7ce3c877125ae',
+  expires_in_token: '15m',
+
+  secret_refresh_token: '2297c03f9e65a1db3d7d071b802c83cf',
+  expires_in_refresh_token: '30d',
+  days_to_expire: 30,
+};
--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -4,6 +4,9 @@ import { inject, injectable } from 'tsyringe';
 
 import { AppError } from '@errors/AppError';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository';
+import auth from '@config/auth';
+import { addDays } from '@utils/dateCompare';
 
 interface IRequest {
   email: string;
@@ -16,13 +19,17 @@ interface IResponse {
     email: string;
   };
   token: string;
+  refresh_token: string;
 }
 
 @injectable()
 export class AuthenticateUserUseCase {
   constructor(
     @inject('UsersRepository')
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+
+    @inject('UsersTokensRepository')
+    private usersTokensRepository: IUsersTokensRepository
   ) {}
 
   async execute({ email, password }: IRequest): Promise<IResponse> {
@@ -38,9 +45,22 @@ export class AuthenticateUserUseCase {
       throw new AppError('Email or password incorrect', 401);
     }
 
-    const token = sign({}, '073dba17fba0dc86eca7ce3c877125ae', {
+    const token = sign({}, auth.secret_token, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn: auth.expires_in_token,
+    });
+
+    const refresh_token = sign({ email }, auth.secret_refresh_token, {
+      subject: user.id,
+      expiresIn: auth.expires_in_refresh_token,
+    });
+
+    const expires_date = addDays(auth.days_to_expire);
+
+    await this.usersTokensRepository.create({
+      expires_date,
+      refresh_token,
+      user_id: user.id,
     });
 
     return {
@@ -49,6 +69,7 @@ export class AuthenticateUserUseCase {
         email: user.email,
       },
       token,
+      refresh_token,
     };
   }
 }
