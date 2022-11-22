2b7ff06b3771362b7e981f6677b5da8fc90c19e9
carlos-hr <carlos.henrique17.chr@gmail.com>
2022-11-21 22:17:35 -0300

    feat: adding avatar to bucket

--- src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
+++ src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -4,6 +4,7 @@ import { resolve } from 'path';
 import fs from 'fs';
 
 import { IStorageProvider } from '../IStorageProvider';
+import mime from 'mime';
 
 export class S3StorageProvider implements IStorageProvider {
   private client: S3;
@@ -16,18 +17,29 @@ export class S3StorageProvider implements IStorageProvider {
     const originalName = resolve(upload.tmpFolder, file);
 
     const fileContent = await fs.promises.readFile(originalName);
+    const ContentType = mime.getType(originalName);
 
-    this.client.putObject({
-      Bucket: `${process.env.AWS_BUCKET}/${folder}`,
-      Key: file,
-      ACL: 'public-read',
-      Body: fileContent,
-      ContentType: 'a',
-    });
-    return 'a';
+    await this.client
+      .putObject({
+        Bucket: `${process.env.AWS_BUCKET}/${folder}`,
+        Key: file,
+        ACL: 'public-read',
+        Body: fileContent,
+        ContentType,
+      })
+      .promise();
+
+    await fs.promises.unlink(originalName);
+
+    return file;
   }
 
   async delete(file: string, folder: string): Promise<void> {
-    throw new Error('Method not implemented.');
+    await this.client
+      .deleteObject({
+        Bucket: `${process.env.AWS_BUCKET}/${folder}`,
+        Key: file,
+      })
+      .promise();
   }
 }
